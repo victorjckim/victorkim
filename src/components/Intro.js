9f5adfb3b160cafa9aade331d3547f3f584e44b0
Victor <vjkim@uci.edu>
2019-05-18 21:46:53 -0700

    removed react-scroll-parallax, will do scrolling with html css

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { Parallax } from "react-scroll-parallax";
-import Image from "../components/Image";
 
 class Intro extends React.Component {
   constructor(props) {
@@ -9,14 +7,7 @@ class Intro extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <Parallax className="custom-class" y={[-20, 20]} tagOuter="figure">
-          <Image src="assets/img/background.jpg" alt="" />
-          <Image src="assets/img/dark.jpg" alt="" />
-        </Parallax>
-      </div>
-    );
+    return <div />;
   }
 }
 
